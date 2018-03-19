82ab724711b3c2e9ba2d06da763c78d8d510dcb0
Adrian Tache <contact@adriantache.com>
2018-03-19 19:01:23 +0200

    Create empty project

---
+++ app/src/main/java/com/adriantache/tour_guide_bucharest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.adriantache.tour_guide_bucharest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
