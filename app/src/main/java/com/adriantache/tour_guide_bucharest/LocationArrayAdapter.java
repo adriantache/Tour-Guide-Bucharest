7a9b11feac62df579de7e2407394070303f84c89
Adrian Tache <contact@adriantache.com>
2018-03-21 14:44:47 +0200

    Update ArrayAdapter with list items

--- app/src/main/java/com/adriantache/tour_guide_bucharest/LocationArrayAdapter.java
+++ app/src/main/java/com/adriantache/tour_guide_bucharest/LocationArrayAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -26,13 +27,17 @@ public class LocationArrayAdapter extends ArrayAdapter<Location> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Location location = getItem(position);
 
-        //todo make list item layout xml
         if (convertView == null) convertView = LayoutInflater.from(getContext()).inflate(R.layout.location,parent,false);
 
-        //todo assign resources to each view in the layout
         ImageView imageView = convertView.findViewById(R.id.location_image);
         imageView.setImageResource(location.getPhotoResID());
 
+        TextView name = convertView.findViewById(R.id.name);
+        name.setText(location.getName());
+
+        TextView address = convertView.findViewById(R.id.address);
+        address.setText(location.getAddress());
+        
         return convertView;
     }
 }
