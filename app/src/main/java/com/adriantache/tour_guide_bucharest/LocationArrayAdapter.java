bcb4114c026b384c16c24adae4e3dd59fe6a9fd7
Adrian Tache <contact@adriantache.com>
2018-03-21 14:42:25 +0200

    Add list item layout xml

--- app/src/main/java/com/adriantache/tour_guide_bucharest/LocationArrayAdapter.java
+++ app/src/main/java/com/adriantache/tour_guide_bucharest/LocationArrayAdapter.java
@@ -3,9 +3,11 @@ package com.adriantache.tour_guide_bucharest;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import java.util.List;
 
@@ -25,11 +27,11 @@ public class LocationArrayAdapter extends ArrayAdapter<Location> {
         Location location = getItem(position);
 
         //todo make list item layout xml
-        //if (convertView == null) convertView = LayoutInflater.from(getContext()).inflate(R.layout.location,parent,false);
+        if (convertView == null) convertView = LayoutInflater.from(getContext()).inflate(R.layout.location,parent,false);
 
         //todo assign resources to each view in the layout
-        //ImageView imageView = listItemView.findViewById(R.id.location_image);
-        //imageView.setImageResource(location.getPhotoResID());
+        ImageView imageView = convertView.findViewById(R.id.location_image);
+        imageView.setImageResource(location.getPhotoResID());
 
         return convertView;
     }
