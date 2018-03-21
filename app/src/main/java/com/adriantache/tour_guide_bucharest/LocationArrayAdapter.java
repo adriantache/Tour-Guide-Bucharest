4bb47866d556fc4d7ecf47495a3f4998183a36e1
Adrian Tache <contact@adriantache.com>
2018-03-21 14:23:08 +0200

    Add custom ArrayAdapter to populate the list in each Fragment

---
+++ app/src/main/java/com/adriantache/tour_guide_bucharest/LocationArrayAdapter.java
@@ -0,0 +1,36 @@
+package com.adriantache.tour_guide_bucharest;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Custom array adapter to populate each list of locations
+ */
+
+public class LocationArrayAdapter extends ArrayAdapter<Location> {
+
+    public LocationArrayAdapter(@NonNull Context context, int resource, @NonNull List<Location> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Location location = getItem(position);
+
+        //todo make list item layout xml
+        //if (convertView == null) convertView = LayoutInflater.from(getContext()).inflate(R.layout.location,parent,false);
+
+        //todo assign resources to each view in the layout
+        //ImageView imageView = listItemView.findViewById(R.id.location_image);
+        //imageView.setImageResource(location.getPhotoResID());
+
+        return convertView;
+    }
+}
